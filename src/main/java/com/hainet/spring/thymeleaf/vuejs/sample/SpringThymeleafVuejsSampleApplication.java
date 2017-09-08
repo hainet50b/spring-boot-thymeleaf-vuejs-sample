2306b76c71bd5f11cb4ed4a78dc60e0591ae7be0
hainet <20400616+hainet@users.noreply.github.com>
2017-09-08 17:11:12 +0900

    Endpointを設定

---
+++ src/main/java/com/hainet/spring/thymeleaf/vuejs/sample/SpringThymeleafVuejsSampleApplication.java
@@ -0,0 +1,20 @@
+package com.hainet.spring.thymeleaf.vuejs.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class SpringThymeleafVuejsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafVuejsSampleApplication.class, args);
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+}
