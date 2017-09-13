1c4e829a147020ffe831182cbdc09295d8cac927
hainet <20400616+hainet@users.noreply.github.com>
2017-09-13 10:57:43 +0900

    Thymeleafを利用してVueインスタンスへ値をバインディングするサンプルを追加

--- src/main/java/com/hainet/spring/thymeleaf/vuejs/sample/SpringThymeleafVuejsSampleApplication.java
+++ src/main/java/com/hainet/spring/thymeleaf/vuejs/sample/SpringThymeleafVuejsSampleApplication.java
@@ -3,6 +3,7 @@ package com.hainet.spring.thymeleaf.vuejs.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
@@ -14,7 +15,8 @@ public class SpringThymeleafVuejsSampleApplication {
     }
 
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", SpringThymeleafVuejsSampleApplication.class.getSimpleName());
         return "index";
     }
 }
