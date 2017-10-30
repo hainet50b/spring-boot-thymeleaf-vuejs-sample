c2eb5b89b9f9ceccceafab958e3758a7eb3324c2
hainet <20400616+hainet@users.noreply.github.com>
2017-10-30 14:37:06 +0900

    パッケージ名の変更

--- src/main/java/com/hainet/spring/boot/thymeleaf/vuejs/sample/SpringBootThymeleafVuejsSampleApplication.java
+++ src/main/java/com/hainet/spring/boot/thymeleaf/vuejs/sample/SpringBootThymeleafVuejsSampleApplication.java
@@ -1,4 +1,4 @@
-package com.hainet.spring.thymeleaf.vuejs.sample;
+package com.hainet.spring.boot.thymeleaf.vuejs.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class SpringThymeleafVuejsSampleApplication {
+public class SpringBootThymeleafVuejsSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringThymeleafVuejsSampleApplication.class, args);
+        SpringApplication.run(SpringBootThymeleafVuejsSampleApplication.class, args);
     }
 
     @GetMapping("")
     public String index(Model model) {
-        model.addAttribute("title", SpringThymeleafVuejsSampleApplication.class.getSimpleName());
+        model.addAttribute("title", SpringBootThymeleafVuejsSampleApplication.class.getSimpleName());
         return "index";
     }
 }
